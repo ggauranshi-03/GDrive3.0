2ec196b1208a83577cb361cc66a7fda2c664627f
Gauranshi Gupta <gauranshigupta2000@gmail.com>
2023-02-19 18:45:06 +0530

    Fileupload changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,26 +13,6 @@ function App() {
   const [modalOpen, setModalOpen] = useState(false);
   useEffect(() => {
     const provider = new ethers.providers.Web3Provider(window.ethereum); // to read data from smart contract
-    //   const loadProvider = async () => {
-    //     if (provider) {
-    //       await provider.send("eth_requestAccounts", []);
-    //       const signer = provider.getSigner(); //to write data on blockchain
-    //       const address = await signer.getAddress();
-    //       let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-    //       const contract = new ethers.Contract(
-    //         contractAddress,
-    //         Upload.abi,
-    //         signer
-    //       );
-    //       console.log(contract);
-    //       setContract(contract);
-    //       setProvider(provider);
-    //     } else {
-    //       console.error("Metamask is not installed !");
-    //     }
-    //   };
-    //   provider && loadProvider();
-    // }, []);
     const loadProvider = async () => {
       if (provider) {
         window.ethereum.on("chainChanged", () => {
@@ -71,6 +51,11 @@ function App() {
       <p style={{ color: "white" }}>
         Account : {account ? account : "Not connected"}
       </p>
+      <FileUpload
+        account={account}
+        provider={provider}
+        contract={contract}
+      ></FileUpload>
     </div>
   );
 }
--- my-app/src/components/FileUpload.js
+++ my-app/src/components/FileUpload.js
@@ -1,4 +1,59 @@
-const FileUpload = () => {
-  return <></>;
+import { useState, usestate } from "react";
+import axios from "axios";
+import "./FileUpload.css";
+const FileUpload = ({ contract, account, provider }) => {
+  const { file, setFile } = useState(null);
+  const { fileName, setFileName } = useState("No image selected");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData("file", file);
+        const resFile = await axios({
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            pinata_api_key: `
+            0483ba1944dc8becbd15`,
+            pinata_secret_api_key: `
+  58a206e573e08007ccb93b881f9c9f0971de7ec123ad1c2b337994e08f6bc6b1`,
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+        //const signer = contract.connect(provider.getSigner());
+        await contract.add(account, ImgHash);
+        alert("Successfully image change");
+        //now change the set file parameters to original state
+        setFileName("No image selected");
+        setFile(null);
+      } catch (e) {
+        alert("Unable to upload");
+      }
+    }
+  };
+  const retrieveFile = () => {};
+  return (
+    <div className="top">
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="file-upload" className="choose">
+          Choose Image
+        </label>
+        <input
+          disabled={!account}
+          type="file"
+          id="file-upload"
+          name="data"
+          onChange={retrieveFile}
+        ></input>
+        <span className="textArea">Image: {fileName}</span>
+        <button type="submit" className="upload" disabled={!file}>
+          Upload File
+        </button>
+      </form>
+    </div>
+  );
 };
 export default FileUpload;
